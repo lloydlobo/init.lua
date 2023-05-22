3e4cdee03393c283e955711a34b1f3b046bc9a6f
Lloyd Lobo <lloydlobo4@gmail.com>
2023-05-22 20:11:05 +0530

    feat: add augroups

--- lua/gg/init.lua
+++ lua/gg/init.lua
@@ -5,5 +5,32 @@ require("gg.set") -- Adds sets to make editor experience better. e.g. automatic 
 -- print("hello from gg") -- debug.
 
 local augroup = vim.api.nvim_create_augroup
-local GgGroup = augroup('Gg',{})
+local gg_group = augroup('Gg', {})
 
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end
+})
+
+autocmd({ "BufWritePre" }, {
+    group = gg_group,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
