724a236e6336e3f1e318f7c6d3e06a9d76f9085c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-05-22 17:29:03 +0530

    feat: remap add advanced key remaps

--- lua/gg/remap.lua
+++ lua/gg/remap.lua
@@ -12,6 +12,19 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- <normal> -> `<leader>+p+v` -> `
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Select and move line below.
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") -- Select and move line above.
 
+vim.keymap.set("n", "Y", "yg$") -- Copy from cursor position to line end.
+vim.keymap.set("n", "J", "mzJ`z") -- keep cursor in same place while appending the line below with space using `J`.
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Half page Jump below and recenter line in window.
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Half page Jump above and recenter line in window.
+vim.keymap.set("n", "n", "nzzzv") -- go forward to search terms and recenter.
+vim.keymap.set("n", "N", "Nzzzv") -- go previous to search terms and recenter.
+
+-- vim.:cmd([[:stop]]) -- CTRL-Z -- Suspend Nvim, like ":stop".
+vim.keymap.set("n", "<leader>gg", function ()
+    vim.cmd([[:terminal gitui]]) -- Open gitui in a terminal buffer.
+end)
+
+
 -- [0 to LSP] 25:25 minute mark
 --
 -- :%s\/(./)noremap(/vim.keymap.set("\1",
